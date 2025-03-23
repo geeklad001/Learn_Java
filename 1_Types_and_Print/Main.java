ef5ec1db812e384f470260843b35b892daca1d07
geeklad13922135 <geeklad13922135@gmail.com>
2025-03-23 18:06:48 +0800

    Learning Types and output

---
+++ 1_Types_and_Print/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ark";
+        // You can use + to combine variables in Java.
+        System.out.print("(print) Hello " + name + '\n');
+        System.out.println("(println) Hello " + name);
+        System.out.printf("(printf) Hello %s\n", name);
+
+        // final is like constant in C/C++
+        final int myInt = 69; // This cannot be changed.
+        // Remember to add an 'f' to indicate that the number is a floating point number
+        float myFloat = 0.8f;
+        // Characters surrounded by single bracket.
+        char myChar = 'S';
+        // Java boolean use lowercase letter as the first letter.
+        boolean myBoolean = true;
+    }
+}
